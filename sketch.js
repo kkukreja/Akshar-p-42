865ac2d7314418a8e1ef20ae8e56470a1dc6cf0f
kkukreja <40955138+kkukreja@users.noreply.github.com>
2022-06-26 04:10:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,126 @@
+var score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
+
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+
+var life =3;
+var score=0;
+var gameState=1
+
+function preload(){
+  gunImg = loadImage("gun1.png")
+  blastImg = loadImage("blast.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
+}
+function setup() {
+  createCanvas(800, 800);
+
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
+  
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
+  
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  heading= createElement("h1");
+  scoreboard= createElement("h1");
+}
+
+function draw() {
+  background("#BDA297");
+  
+  heading.html("Life: "+life)
+  heading.style('color:red'); 
+  heading.position(150,20)
+
+  scoreboard.html("Score: "+score)
+  scoreboard.style('color:red'); 
+  scoreboard.position(width-200,20)
+
+  if(gameState===1){
+    gun.y=mouseY  
+
+    if (frameCount % 80 === 0) {
+      drawblueBubble();
+    }
+
+    if (frameCount % 100 === 0) {
+      drawredBubble();
+    }
+
+    if(keyDown("space")){
+      shootBullet();
+    }
+
+    if (blueBubbleGroup.collide(backBoard)){
+      handleGameover(blueBubbleGroup);
+    }
+    if (redBubbleGroup.collide(backBoard)) {
+      handleGameover(redBubbleGroup);
+    }
+    
+    if(blueBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(blueBubbleGroup);
+    }
+
+    if(redBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(redBubbleGroup);
+    }
+
+    drawSprites();
+  }
+    
+  
+}
+
+function drawblueBubble(){
+  //code to create the blue bubbles
+}
+function drawredBubble(){
+  //code to create red bubbles
+}
+
+function shootBullet(){
+  bullet= createSprite(150, width/2, 50,20)
+  bullet.y= gun.y-20
+  bullet.addImage(bulletImg)
+  bullet.scale=0.12
+  bullet.velocityX= 7
+  bulletGroup.add(bullet)
+}
+
+function handleBubbleCollision(bubbleGroup){
+    //code to handle bubble collision
+}
+
+function handleGameover(bubbleGroup){
+  
+    life=life-1;
+    bubbleGroup.destroyEach();
+    
+
+    if (life === 0) {
+      gameState=2
+      
+      swal({
+        title: `Game Over`,
+        text: "Oops you lost the game....!!!",
+        text: "Your Score is " + score,
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+        imageSize: "100x100",
+        confirmButtonText: "Thanks For Playing"
+      });
+    }
+  
+}
